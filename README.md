b3ed5cfd192ac5920e0dba16f8596d41c58d7816
Knarkzel <knarkzel@gmail.com>
2022-09-27 19:03:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# oddos
+
+```
+git clone git@git.sr.ht:~knarkzel/oddos
+cd oddos
+nix develop
+zig build run
+```
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-22.05";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    overlays = [
+      # Zig overlay
+      (final: prev: {
+        zigpkgs = inputs.zig-overlay.packages.${prev.system};
+      })
+    ];
+    # Our supported systems are the same supported systems as the Zig binaries
+    systems = builtins.attrNames inputs.zig-overlay.packages;
+  in
+    flake-utils.lib.eachSystem systems (
+      system: let
+        pkgs = import nixpkgs {inherit overlays system;};
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [pkgs.zigpkgs.master];
+        };
+      }
+    );
+}
