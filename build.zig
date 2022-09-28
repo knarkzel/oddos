820fe9a4b23bcc29540b862266b2daed7142dc36
Knarkzel <knarkzel@gmail.com>
2022-09-28 18:09:52 +0200

    Added zig build iso command

--- build.zig
+++ build.zig
@@ -16,9 +16,11 @@ pub fn build(b: *std.build.Builder) !void {
     kernel.code_model = .kernel;
     kernel.install();
 
+    // Kernel step
     const kernel_step = b.step("kernel", "Build the kernel");
     kernel_step.dependOn(&kernel.install_step.?.step);
 
+    // Run step
     const run_cmd = b.addSystemCommand(&.{
         "qemu-system-i386",
         "-kernel",
@@ -28,4 +30,17 @@ pub fn build(b: *std.build.Builder) !void {
 
     const run_step = b.step("run", "Run the kernel");
     run_step.dependOn(&run_cmd.step);
+
+    // Iso step
+    const dir_cmd = b.addSystemCommand(&.{ "mkdir", "-p", "zig-out/iso/boot/grub" });
+    const elf_cmd = b.addSystemCommand(&.{ "cp", "zig-out/bin/oddos.elf", "zig-out/iso/boot" });
+    const cfg_cmd = b.addSystemCommand(&.{ "cp", "src/grub.cfg", "zig-out/iso/boot/grub" });
+    const grub_cmd = b.addSystemCommand(&.{ "grub-mkrescue", "-o", "zig-out/bin/oddos.iso", "zig-out/iso" });
+
+    const iso_step = b.step("iso", "Build the iso");
+    iso_step.dependOn(&grub_cmd.step);
+    grub_cmd.step.dependOn(&cfg_cmd.step);
+    cfg_cmd.step.dependOn(&elf_cmd.step);
+    elf_cmd.step.dependOn(&dir_cmd.step);
+    dir_cmd.step.dependOn(kernel_step);
 }
