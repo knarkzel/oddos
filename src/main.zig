02eb619ae693482615cd3bbbc01ba36c0d5cc05e
Knarkzel <knarkzel@gmail.com>
2022-10-06 01:14:33 +0200

    ITS WOrking

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,29 @@ const gdt = @import("system/gdt.zig");
 const idt = @import("system/idt.zig");
 const Serial = @import("driver/Serial.zig");
 const Terminal = @import("driver/Terminal.zig");
-export const isr_handler = @import("system/isr.zig").isr_handler;
+
+const Registers = struct {
+    ds: u32,
+    edi: u32,
+    esi: u32,
+    ebp: u32,
+    esp: u32,
+    ebx: u32,
+    edx: u32,
+    ecx: u32,
+    eax: u32,
+    number: u32,
+    error_code: u32,
+    eip: u32,
+    cs: u32,
+    eflags: u32,
+    useresp: u32,
+    ss: u32,
+};
+
+export fn isr_handler() void {
+    Terminal.write("\nINTERRUPT OCCURRED");
+}
 
 pub fn init() void {
     gdt.init();
@@ -18,5 +40,4 @@ pub fn main() void {
     Terminal.write("> ");
     Terminal.setColor(.White, .Black);
     asm volatile ("int $0x3");
-    asm volatile ("int $0x4");
 }
