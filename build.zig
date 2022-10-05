64d934a4e78d5081b0424b5454dde0f69d805955
Knarkzel <knarkzel@gmail.com>
2022-10-06 01:44:04 +0200

    Fix release modes

--- build.zig
+++ build.zig
@@ -10,6 +10,7 @@ pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
 
     const kernel = b.addExecutable("oddos.elf", "src/init.zig");
+    kernel.want_lto = false;
     kernel.addAssemblyFile("src/arch/x86/gdt.s");
     kernel.addAssemblyFile("src/arch/x86/interrupts.s");
     kernel.setTarget(target);
